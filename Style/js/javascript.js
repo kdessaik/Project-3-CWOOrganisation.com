58c61772b00f0fa3dac23dc4e1d0efceb0ec6d48
Kambale Kibeho <dessaikibeho@gmail.com>
2022-04-22 18:30:34 +0300

    myChange

--- Style/js/javascript.js
+++ Style/js/javascript.js
@@ -1,3 +1,6 @@
+//Grap class
+const NavBar=document.querySelector('.navbar-toggler-icon')
+const closeBar=document.querySelector('.close')
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
@@ -7,3 +10,18 @@ function openNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
   }
+
+  function NavBar1() {
+    document.getElementById('section3').style.display="none";
+    document.getElementById('first-logo1').style.top='25%'
+    
+  }
+    
+  
+  function NavBar2(){
+     document.getElementById('section3').style.display="flex";
+     document.getElementById('first-logo1').style.top='500px'
+  }
+  NavBar.addEventListener("click",NavBar2)
+  closeBar.addEventListener('click',NavBar1)
+
