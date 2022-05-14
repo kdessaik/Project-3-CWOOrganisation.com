e520b35def1b83d14d2be9384aceec0431b36769
Kambale Kibeho <dessaikibeho@gmail.com>
2022-05-14 16:40:21 +0300

    kdl

--- pages/Generator/GeneratorsPhotos.js
+++ pages/Generator/GeneratorsPhotos.js
@@ -11,6 +11,32 @@ var imgs7=document.createElement("img")
 var imgs8=document.createElement("img")
 var imgs9=document.createElement("img")
 var imgs10=document.createElement("img")
+var imgs11=document.createElement("img")
+var imgs12=document.createElement("img")
+var imgs13=document.createElement("img")
+var imgs14=document.createElement("img")
+var imgs15=document.createElement("img")
+var imgs16=document.createElement("img")
+var imgs17=document.createElement("img")
+var imgs18=document.createElement("img")
+var imgs19=document.createElement("img")
+var imgs20=document.createElement("img")
+var imgs21=document.createElement("img")
+var imgs22=document.createElement("img")
+var imgs23=document.createElement("img")
+var imgs24=document.createElement("img")
+var imgs25=document.createElement("img")
+var imgs26=document.createElement("img")
+var imgs27=document.createElement("img")
+var imgs28=document.createElement("img")
+var imgs29=document.createElement("img")
+var imgs30=document.createElement("img")
+var imgs31=document.createElement("img")
+var imgs32=document.createElement("img")
+var imgs33=document.createElement("img")
+var imgs34=document.createElement("img")
+var imgs35=document.createElement("img")
+
 
 function ourPhotos(){
      
@@ -25,6 +51,33 @@ function ourPhotos(){
     imgs8.src="./Generator/images/IMG_20211013_124447_0.jpg"
     imgs9.src="./Generator/images/IMG_20211013_124655_951.jpg"
     imgs10.src="./Generator/images/IMG_20211013_123103_808.jpg"
+    imgs11.src="./Generator/images/NewPhoto/Copy of IMG_0353.jpg"
+    imgs12.src="./Generator/images/NewPhoto/Copy of IMG_0361.jpg"
+    imgs13.src="./Generator/images/NewPhoto/Copy of IMG_0431.jpg"
+    imgs14.src="./Generator/images/NewPhoto/Copy of IMG_0431 - Copy.jpg"
+    imgs15.src="./Generator/images/NewPhoto/IMG_0441.jpg"
+    imgs16.src="./Generator/images/NewPhoto/IMG_0461.jpg"
+    imgs17.src="./Generator/images/NewPhoto/IMG_0460.jpg"
+    imgs18.src="./Generator/images/NewPhoto/IMG_0521.jpg"
+    
+    imgs20.src="./Generator/images/NewPhoto/IMG_20200411_104335_235.jpg"
+    imgs21.src="./Generator/images/NewPhoto/IMG_20200810_103456_465.jpg"
+    imgs22.src="./Generator/images/NewPhoto/IMG_20200810_103945_578.jpg"
+    imgs23.src="./Generator/images/NewPhoto/IMG_20200811_103157_205.jpg"
+    imgs24.src="./Generator/images/NewPhoto/IMG_20200815_095213_279 - Copy.jpg"
+    imgs25.src="./Generator/images/NewPhoto/IMG_20200815_110846_800.jpg"
+    imgs26.src="./Generator/images/NewPhoto/IMG_20210211_171343_514.jpg"
+    imgs27.src="./Generator/images/NewPhoto/IMG_20210304_145259_022.jpg"
+    imgs28.src="./Generator/images/NewPhoto/IMG_20210522_105704_286.jpg"
+    imgs29.src="./Generator/images/NewPhoto/IMG_20210522_105926_318.jpg"
+    imgs30.src="./Generator/images/NewPhoto/IMG_20210522_105956_476.jpg"
+    imgs31.src="./Generator/images/NewPhoto/IMG_20210522_110308_572.jpg"
+    imgs32.src="./Generator/images/NewPhoto/IMG_20210522_110333_473.jpg"
+    imgs33.src="./Generator/images/NewPhoto/IMG_20210522_110338_079.jpg"
+    imgs34.src="./Generator/images/NewPhoto/IMG_20210522_110730_325.jpg"
+    imgs35.src="./Generator/images/NewPhoto/IMG_20210522_110836_610.jpg"
+    
+
 
     allPhotos.appendChild(imgs)
     allPhotos.appendChild(imgs1)
@@ -37,6 +90,32 @@ function ourPhotos(){
     allPhotos.appendChild(imgs8)
     allPhotos.appendChild(imgs9)
     allPhotos.appendChild(imgs10)
+    allPhotos.appendChild(imgs11)
+    allPhotos.appendChild(imgs12)
+    allPhotos.appendChild(imgs13)
+    allPhotos.appendChild(imgs14)
+    allPhotos.appendChild(imgs15)
+    allPhotos.appendChild(imgs16)
+    allPhotos.appendChild(imgs17)
+    allPhotos.appendChild(imgs18)
+    allPhotos.appendChild(imgs19)
+    allPhotos.appendChild(imgs20)
+    allPhotos.appendChild(imgs21)
+    allPhotos.appendChild(imgs22)
+    allPhotos.appendChild(imgs23)
+    allPhotos.appendChild(imgs24)
+    allPhotos.appendChild(imgs25)
+    allPhotos.appendChild(imgs26)
+    allPhotos.appendChild(imgs27)
+    allPhotos.appendChild(imgs28)
+    allPhotos.appendChild(imgs29)
+    allPhotos.appendChild(imgs30)
+    allPhotos.appendChild(imgs31)
+    allPhotos.appendChild(imgs32)
+    allPhotos.appendChild(imgs33)
+    allPhotos.appendChild(imgs34)
+    allPhotos.appendChild(imgs35)
+
     
 }
 ourPhotos()
